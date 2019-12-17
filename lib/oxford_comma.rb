97e3cfeecff5ca16d4a36d20b2489769a2f369f6
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 00:25:35 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
   return array.first if array.size == 1 
   
-  if array.size == 2 
-    array.insert(1, "and").join("")
+  if array.length < 2 
+    array.join 
     
     
   end 
