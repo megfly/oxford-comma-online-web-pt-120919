e8ef9f3ccfe21d94c8b04c21061672bfa7d2401d
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 00:43:50 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join('')
+  
+  else if array.length == 2 
+    array.join (' and ') 
+  else if array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    array.join(', ')
+  else 
+    nil 
+  end 
+end 
 end
+end 
