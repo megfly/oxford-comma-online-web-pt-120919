b85c0f6cfab6aa8e5e5d5b95af496444f2241b12
Meghan Flynn <mflynn333@gmail.com>
2019-12-17 00:35:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ def oxford_comma(array)
     array.join 
   else if array.length == 2 
     array.insert(1, "and").join(" ")
-    
-  array.split(", ")
-  end 
+  else 
+    string = ", and " + array.pop
+    string = array.join (",") << string 
 end 
 end
